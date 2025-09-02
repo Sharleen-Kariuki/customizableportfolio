33fa4d5cec5f7d6ba415273bb929375a300ba0a2
Sharleen-Kariuki <sharleenwambui28@gmail.com>
2025-09-02 15:10:17 +0300

    legal blog

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,7 +3,6 @@ import './Blog.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import blog_data from '../../assets/blog_data.js'
 
-
 const Blog = () => {
   return (
     <div id="blog" className='blog'>
@@ -13,13 +12,23 @@ const Blog = () => {
       </div>
       <div className="blog-container">
         {blog_data.map((blog, index) => {
-            return <div key={index} className='blog-format'>
-                <h3>{blog.s_no}</h3>
-                <h2>{blog.s_name}</h2>
-                <p>{blog.s_desc}</p>
+          return (
+            <div key={index} className='blog-flip-card'>
+              <div className='blog-flip-card-inner'>
+                {/* Front Side */}
+                <div className='blog-flip-card-front'>
+                  <h3>{blog.s_no}</h3>
+                  <h2>{blog.s_name}</h2>
+                  <p>{ blog.s_desc}</p>
                 </div>
-
-          })}
+                {/* Back Side */}
+                <div className='blog-flip-card-back'>
+                  <p>{blog.s_stats}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
