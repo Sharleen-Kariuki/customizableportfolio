b8f98949c56564442d1c293d0b0e33c4ecdcf24a
Sharleen-Kariuki <sharleenwambui28@gmail.com>
2025-09-02 15:38:39 +0300

    Statistics messages

--- src/assets/blog_data.js
+++ src/assets/blog_data.js
@@ -3,25 +3,25 @@ const blog_data = [
     s_no: "01",
     s_name: "What to Do After Receiving a Demand Letter",
     s_desc: "Understand your rights and options when served with a demand letter, including how to respond appropriately and when to seek legal advice.",
-    s_stats: "The post What to Do After Receiving a Demand Letter appeared first on Jefferson Waithaka."
+    s_stats: "In Kenya, a demand letter is a serious notice that requires prompt legal attention. Consult an advocate to guide you on whether to settle, negotiate, or dispute the claim. For more details, contact me."
   },
   {
     s_no: "02",
     s_name: "How to Legally Protect Your Business Idea",
     s_desc: "Explore essential steps for safeguarding your business concept through intellectual property laws, contracts, and NDAs.",
-     s_stats: "The post What to Do After Receiving a Demand Letter appeared first on Jefferson Waithaka."
+     s_stats: "You can only protect your business idea once it is developed into a tangible form such as an invention, brand, or creative work. Legal tools like patents, trademarks, copyrights, and NDAs help secure ownership and prevent misuse. For professional guidance on protecting your idea, contact me today."
   },
   {
     s_no: "03",
     s_name: "Legal Checklist Before Signing a Contract",
     s_desc: "A step-by-step guide to reviewing contracts—what clauses to watch out for, red flags, and tips to negotiate better terms.",
-     s_stats: "The post What to Do After Receiving a Demand Letter appeared first on Jefferson Waithaka."
+     s_stats: "Before signing a contract in Kenya, always read it carefully to understand the terms, obligations, timelines, and penalties. Confirm that all agreed details are included in writing and ensure the contract complies with Kenyan law. For a detailed legal checklist and professional review before you commit, contact me.."
   },
   {
     s_no: "04",
     s_name: "What Happens If You Die Without a Will?",
     s_desc: "Learn about intestate succession laws and why drafting a will can protect your loved ones and avoid family disputes.",
-      s_stats: "The post What to Do After Receiving a Demand Letter appeared first on Jefferson Waithaka."
+      s_stats: "In Kenya, if you die without a will, your estate is distributed under the rules of intestacy, often leading to delays and family disputes. The court appoints administrators to manage and share your property according to set rules, not your personal wishes. For advice on securing your estate through a will, contact me."
   }
 ]
  
